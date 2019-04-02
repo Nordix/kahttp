260754ced2c3c464215d3db7f93aabfad685d2e7
Lars Ekman <lars.g.ekman@est.tech>
2019-04-02 13:34:04 +0200

    Added code and doc

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
-# kahttp
-Keep-alive http test program
+# kahttp - Keep-alive http test program
+
+This test program is intended for testing many simultaneous
+(keep-alive) http connections. It is not intended for throughput or
+latency measurements but is designed to test the limit for how many
+simultaneous client connectons a http server or proxy can handle.
+
+
+## Build
+
+```
+go get github.com/Nordix/mconnect
+cd $GOPATH/src/github.com/Nordix/kahttp
+ver=$(date +%F:%T)
+CGO_ENABLED=0 GOOS=linux go install -a \
+  -ldflags "-extldflags '-static' -X main.version=$ver" \
+  github.com/Nordix/kahttp/cmd/kahttp
+strip $GOPATH/bin/kahttp
+```
+
+## Usage
+
+Local usage;
+
+```
+# Start server;
+kahttp -server -address [::]:5080
+# In another shell;
+wget -q -O - http://127.0.0.1:5080/
+kahttp -address http://127.0.0.1:5080/ -monitor -rate 400 -nconn 40 -timeout 10s | jq .
+# In yet another shell;
+tcpdump -ni lo 'tcp[tcpflags] == tcp-syn'
+# Or for ipv6;
+tcpdump -ni lo tcp and 'ip6[13+40]&0x2!=0'
+```
+
+One way to verify that keep-alive is used done is to trace the SYN
+packets with `tcpdump`. If there are an initial burst of and then
+silence keep-alive is used. A steady stream os SYNs indicates that
+keep-alive is not used.
+
+
+
+## Measure keep-alive connections
+
+`Kahttp` starts a number of http clients according to the `-nclients`
+parameter. Each of these clients will make periodic http GET requests
+towards the URL specified by the `-address` parameter. The `-rate`
+parameter specifies the **total** number of http requests per
+second. This is divided among the clients so a rate of 400 and 40
+clients will result in 10 requests per second per client. The
+keep-alive time is limited (but different) on servers so to make sure
+connections are kept a certain rate per client must be maintained.
+
+The key metric for measuring keep-alive connections is `Dials`. This
+is the number of actual TCP connects that the clients do. Below is an
+example where all connections are kept;
+
+```
+> kahttp -address http://127.0.0.1:5080/ -monitor -rate 400 -nclients 40 -timeout 10s | jq .
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 399/399/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 799/799/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 1199/1199/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 1600/1600/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 2000/2000/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 2400/2400/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 2800/2800/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 3200/3200/0
+Clients act/fail/Dials: 40/0/40, Packets send/rec/dropped: 3600/3600/0
+{
+  "Started": "2019-04-02T13:02:06.815903366+02:00",
+  "Duration": 10000112870,
+  "Rate": 400,
+  "Clients": 40,
+  "Dials": 40,
+  "FailedConnections": 0,
+  "Sent": 3999,
+  "Received": 3999,
+  "Dropped": 0,
+  "FailedConnects": 0
+}
+```
+
+In this case we have 40 clients making 3600 requests in total but the
+number of `Dials` is 40, so each client make only one connection which
+is kept alive for the entire run.
+
+This shows the case where there is no keep-alive;
+
+```
+> kahttp -address http://127.0.0.1:5080/ -monitor -rate 400 -nclients 40 -timeout 10s | jq .
+Clients act/fail/Dials: 40/0/400, Packets send/rec/dropped: 399/399/0
+Clients act/fail/Dials: 40/0/800, Packets send/rec/dropped: 799/799/0
+Clients act/fail/Dials: 40/0/1200, Packets send/rec/dropped: 1199/1199/0
+Clients act/fail/Dials: 40/0/1600, Packets send/rec/dropped: 1599/1599/0
+Clients act/fail/Dials: 40/0/2000, Packets send/rec/dropped: 1999/1999/0
+Clients act/fail/Dials: 40/0/2400, Packets send/rec/dropped: 2400/2400/0
+Clients act/fail/Dials: 40/0/2800, Packets send/rec/dropped: 2799/2799/0
+Clients act/fail/Dials: 40/0/3200, Packets send/rec/dropped: 3200/3200/0
+Clients act/fail/Dials: 40/0/3600, Packets send/rec/dropped: 3600/3600/0
+{
+  "Started": "2019-04-02T13:16:37.443570632+02:00",
+  "Duration": 10001866073,
+  "Rate": 400,
+  "Clients": 40,
+  "Dials": 4000,
+  "FailedConnections": 0,
+  "Sent": 4000,
+  "Received": 4000,
+  "Dropped": 0,
+  "FailedConnects": 0
+}
+```
+
+The number of `Dials` is equal to the number of `Sent` requests. This
+means that no connection is kept alive.
+
+
+## Multiple source addresses
+
+For testing of very many connections it is essential that the
+connections originates from many sources. If just one (prominent)
+source is used there will likely be a shortage of connection resources
+since the src/dest addresses are always the same, only the ports can
+be varied. The resource problem does not have to occur on the
+endpoints but can be in network elements (NE) in between such as
+load-balancers or NAT boxes.
+
+To use many source addresses you must first assign a CIDR to the
+loopback interface as described for
+[mconnect](https://github.com/Nordix/mconnect#many-source-addresses). Then
+use;
+
+```
+kahttp -address http://127.0.0.1:5080/ -monitor -rate 400 -nconn 40 \
+  -timeout 10s -scrcidr 222.222.222.0/24
+```
---
+++ cmd/kahttp/main.go
@@ -0,0 +1,414 @@
+// Project page; https://github.com/Nordix/kahttp/
+// LICENSE; MIT. See the "LICENSE" file in the Project page.
+// Copyright (c) 2019, Nordix Foundation
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"golang.org/x/time/rate"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"sync"
+	"sync/atomic"
+	"syscall"
+	"time"
+	"net/http"
+	"github.com/Nordix/mconnect/pkg/rndip"
+)
+
+var version string = "unknown"
+
+const helptext = `
+Kahttp attempts to setup many http connections and keep them alive.
+
+Kahttp has 2 modes;
+
+ 1. Server - simple server
+ 2. Client - traffic generator
+
+Options;
+`
+
+type config struct {
+	isServer  *bool
+	addr      *string
+	nconn     *int
+	version   *bool
+	timeout   *time.Duration
+	monitor   *bool
+	psize     *int
+	rate      *float64
+	stats     *string
+	srccidr   *string
+	rndip     *rndip.Rndip
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), helptext)
+		flag.PrintDefaults()
+	}
+
+	var cmd config
+	cmd.isServer = flag.Bool("server", false, "Act as server")
+	cmd.addr = flag.String("address", "http://127.0.0.1:5080/", "Server address")
+	cmd.nconn = flag.Int("nclients", 1, "Number of http clients")
+	cmd.version = flag.Bool("version", false, "Print version and quit")
+	cmd.timeout = flag.Duration("timeout", 10*time.Second, "Timeout")
+	cmd.monitor = flag.Bool("monitor", false, "Monitor")
+	psize := 1024
+	cmd.psize = &psize
+	cmd.rate = flag.Float64("rate", 10.0, "Rate in http-requests/Second")
+	cmd.srccidr = flag.String("srccidr", "", "Source CIDR")
+
+	flag.Parse()
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if *cmd.version {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	if *cmd.isServer {
+		os.Exit(cmd.serverMain())
+	} else {
+		os.Exit(cmd.clientMain())
+	}
+}
+
+
+// ----------------------------------------------------------------------
+// Client
+
+type ctConn interface {
+	Connect(ctx context.Context, address string) error
+	Run(ctx context.Context, s *statistics) error
+}
+
+// TODO: Use the "connstats" struct in the statistics section
+type connData struct {
+	id               uint32
+	psize            int
+	rate             float64
+	sent             uint32
+	nPacketsReceived uint32
+	nPacketsDropped  uint32
+	err              error
+	started          time.Time
+	connected        time.Time
+	ended            time.Time
+	nFailedConnect   uint
+	localAddr        net.Addr
+}
+
+var cData []connData
+var nConn uint32
+
+func (c *config) clientMain() int {
+
+	s := newStats(*c.timeout, *c.rate, *c.nconn, uint32(*c.psize))
+	rand.Seed(time.Now().UnixNano())
+
+	// The connection array may contain re-connects
+	cData = make([]connData, *c.nconn*10)
+
+	deadline := time.Now().Add(*c.timeout)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+
+	if *c.srccidr != "" {
+		var err error
+		c.rndip, err = rndip.New(*c.srccidr)
+		if err != nil {
+			log.Fatal("Set source failed:", err)
+		}
+	}
+	
+	var wg sync.WaitGroup
+	wg.Add(*c.nconn)
+	for i := 0; i < *c.nconn; i++ {
+		go c.client(ctx, &wg, s)
+	}
+
+	if *c.monitor {
+		go monitor(s)
+	}
+
+	wg.Wait()
+
+	s.reportStats()
+
+	return 0
+}
+
+func (c *config) client(ctx context.Context, wg *sync.WaitGroup, s *statistics) {
+	defer wg.Done()
+
+	for {
+
+		// Check that we have > 2sec until deadline
+		deadline, _ := ctx.Deadline()
+		if deadline.Sub(time.Now()) < 2*time.Second {
+			return
+		}
+
+		// Initiate a new http client
+		id := atomic.AddUint32(&nConn, 1) - 1
+		if int(id) >= len(cData) {
+			log.Fatal("Too many re-connects", id)
+		}
+		cd := &cData[id]
+		cd.id = id
+		cd.started = time.Now()
+		cd.psize = *c.psize
+		cd.rate = *c.rate / float64(*c.nconn)
+		if c.rndip != nil {
+			sadr := fmt.Sprintf("%s:0", c.rndip.GetIPString())
+			if saddr, err := net.ResolveTCPAddr("tcp", sadr); err != nil {
+				log.Fatal(err)
+				return
+			} else {
+				cd.localAddr = saddr
+			}
+		}
+
+		conn := newHttpConn(cd)
+
+		// Connect with re-try and back-off
+		backoff := 100 * time.Millisecond
+		err := conn.Connect(ctx, *c.addr)
+		for err != nil {
+			time.Sleep(backoff)
+			if backoff < time.Second {
+				backoff += 100 * time.Millisecond
+			}
+			if deadline.Sub(time.Now()) < 2*time.Second {
+				cd.ended = s.Started.Add(s.Duration)
+				return
+			}
+			cd.nFailedConnect++
+			err = conn.Connect(ctx, *c.addr)
+		}
+		cd.connected = time.Now()
+
+		cd.err = conn.Run(ctx, s)
+		if cd.err == nil {
+			// NOTE: The connection *will* stop prematurely if the
+			// next packet can't be sent before the dead-line. However
+			// the stasistics should show that the connection exists
+			// to the test end.
+			cd.ended = s.Started.Add(s.Duration)
+			return // OK return
+		}
+		cd.ended = time.Now()
+
+		s.failedConnection(1)
+	}
+
+}
+
+func monitor(s *statistics) {
+	deadline := s.Started.Add(s.Duration - 1500*time.Millisecond)
+	for time.Now().Before(deadline) {
+		time.Sleep(time.Second)
+		var nAct, nConnecting uint
+		for _, cd := range cData[:nConn] {
+			if cd.err == nil {
+				if cd.connected.IsZero() {
+					nConnecting++
+				} else {
+					nAct++
+				}
+			}
+		}
+		fmt.Fprintf(
+			os.Stderr,
+			"Clients act/fail/Dials: %d/%d/%d, Packets send/rec/dropped: %d/%d/%d\n",
+			nAct, s.FailedConnections, nDials, s.Sent, s.Received, s.Dropped)
+	}
+}
+
+func newLimiter(ctx context.Context, r float64, psize int) *rate.Limiter {
+	// Allow some burstiness but drain the bucket from start
+	// Introduce some ramndomness to spread traffic
+	lim := rate.NewLimiter(rate.Limit(r*1024.0), psize*10)
+	if lim.WaitN(ctx, rand.Intn(psize)) != nil {
+		return nil
+	}
+	for lim.AllowN(time.Now(), psize) {
+	}
+	return lim
+}
+
+
+// ----------------------------------------------------------------------
+// Http Connection
+
+type httpConn struct {
+	cd   *connData
+	address string
+	client *http.Client
+}
+
+func newHttpConn(cd *connData) ctConn {
+	return &httpConn{
+		cd: cd,
+	}
+}
+
+func (c *httpConn) Connect(ctx context.Context, address string) error {
+	tr := &http.Transport{
+		MaxIdleConns:       1000,
+		IdleConnTimeout:    30 * time.Minute,
+		DisableCompression: true,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+			LocalAddr: c.cd.localAddr,
+			Control: myControl,
+		}).DialContext,
+	}
+	c.client = &http.Client{Transport: tr}
+	c.address = address
+	return nil
+}
+// https://stackoverflow.com/questions/52423335/define-tcp-socket-options/52426887
+var nDials uint32
+func myControl(network, address string, c syscall.RawConn) error {
+	atomic.AddUint32(&nDials, 1)
+	return nil
+}
+func (c *httpConn) Run(ctx context.Context, s *statistics) error {
+
+	lim := newLimiter(ctx, c.cd.rate, c.cd.psize)
+	if lim == nil {
+		return nil
+	}
+
+	for {
+		
+		if lim.WaitN(ctx, c.cd.psize) != nil {
+			break
+		}
+
+		resp, err := c.client.Get(c.address)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		s.sent(1)
+
+		for lim.AllowN(time.Now(), c.cd.psize) {
+			s.dropped(1)
+		}
+
+		if _, err := ioutil.ReadAll(resp.Body); err != nil {
+			return err
+		}
+
+		s.received(1)
+	}
+
+	return nil
+}
+
+// ----------------------------------------------------------------------
+// Server
+
+type myHandler int
+func (c *config) serverMain() int {
+	s := &http.Server{
+		Addr:           *c.addr,
+		Handler:        myHandler(1),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		IdleTimeout:    10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+	return 0
+}
+
+func (x myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Method: %s\n", r.Method)
+	fmt.Fprintf(w, "URL: %s\n", r.URL)
+	fmt.Fprintf(w, "Proto: %s\n", r.Proto)
+	fmt.Fprintf(w, "ContentLength: %d\n", r.ContentLength)
+	fmt.Fprintf(w, "TransferEncoding: %v\n", r.TransferEncoding)
+	fmt.Fprintf(w, "Host: %s\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr: %s\n", r.RemoteAddr)
+	fmt.Fprintf(w, "RequestURI: %s\n", r.RequestURI)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "%s: %s\n", k, v)
+	}
+}
+
+// ----------------------------------------------------------------------
+// Statistics
+
+type statistics struct {
+	Started           time.Time
+	Duration          time.Duration
+	Rate              float64
+	Clients           int
+	Dials             uint32
+	FailedConnections uint32
+	Sent              uint32
+	Received          uint32
+	Dropped           uint32
+	FailedConnects    uint
+}
+
+type sample struct {
+	Time     time.Duration
+	Sent     uint32
+	Received uint32
+	Dropped  uint32
+}
+
+func newStats(
+	duration time.Duration,
+	rate float64,
+	connections int,
+	packetSize uint32) *statistics {
+
+	s := &statistics{
+		Started:     time.Now(),
+		Duration:    duration,
+		Rate:        rate,
+		Clients: connections,
+	}
+	return s
+}
+
+func (s *statistics) sent(n uint32) {
+	atomic.AddUint32(&s.Sent, n)
+}
+func (s *statistics) received(n uint32) {
+	atomic.AddUint32(&s.Received, n)
+}
+func (s *statistics) dropped(n uint32) {
+	atomic.AddUint32(&s.Dropped, n)
+}
+func (s *statistics) failedConnection(n uint32) {
+	atomic.AddUint32(&s.FailedConnections, n)
+}
+
+func (s *statistics) reportStats() {
+	s.Duration = time.Now().Sub(s.Started)
+	s.Dials = nDials
+	json.NewEncoder(os.Stdout).Encode(s)
+}
+
